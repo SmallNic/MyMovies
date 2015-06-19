9b4d470545d05a9e9e74cd83f5f6824339c950d0
SmallNic <nas231@nyu.edu>
2015-06-19 00:49:04 -0400

    changing mongolab URI to make it work

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 var mongoose = require("mongoose")
 var options = { server:  { socketOptions: { keepAlive: 1, connectTimeoutMS:30000 } },
                 replset: { socketOptions: { keepAlive: 1, connectTimeoutMS:30000 } } }
-var mongodbUri = "mongodb://<dbuser>:<dbpassword>@ds047692.mongolab.com:47692/heroku_m2gr5q80"
+var mongodbUri = "mongodb://ds047692.mongolab.com:47692/heroku_m2gr5q80"
 mongoose.connect(mongodbUri, options)  //For deployment to Heroku - to be uncommented out later
 // mongoose.connect("mongodb://localhost/my_movies")
 module.exports = mongoose
